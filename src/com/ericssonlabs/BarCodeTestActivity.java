c135fd4be66e6d51bfe2e2d5a2e2204e306b6478
S-sy <android_ssy@126.com>
2016-03-08 11:15:40 +0800

    zxing二维码扫描与生成

---
+++ src/com/ericssonlabs/BarCodeTestActivity.java
@@ -0,0 +1,95 @@
+package com.ericssonlabs;
+
+import java.io.UnsupportedEncodingException;
+
+import com.google.zxing.WriterException;
+import com.zxing.activity.CaptureActivity;
+import com.zxing.encoding.EncodingHandler;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class BarCodeTestActivity extends Activity {
+    /** Called when the activity is first created. */
+	private TextView resultTextView;
+	private EditText qrStrEditText;
+	private ImageView qrImgImageView;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        resultTextView = (TextView) this.findViewById(R.id.tv_scan_result);
+        qrStrEditText = (EditText) this.findViewById(R.id.et_qr_string);
+        qrImgImageView = (ImageView) this.findViewById(R.id.iv_qr_image);
+        
+        Button scanBarCodeButton = (Button) this.findViewById(R.id.btn_scan_barcode);
+        scanBarCodeButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				//打开扫描界面扫描条形码或二维码
+				Intent openCameraIntent = new Intent(BarCodeTestActivity.this,CaptureActivity.class);
+				startActivityForResult(openCameraIntent, 0);
+			}
+		});
+        
+        Button generateQRCodeButton = (Button) this.findViewById(R.id.btn_add_qrcode);
+        generateQRCodeButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				try {
+					String contentString = qrStrEditText.getText().toString();
+					if (!contentString.equals("")) {
+						//根据字符串生成二维码图片并显示在界面上，第二个参数为图片的大小（350*350）
+						Bitmap qrCodeBitmap = EncodingHandler.createQRCode(contentString, 350);
+						qrImgImageView.setImageBitmap(qrCodeBitmap);
+					}else {
+						Toast.makeText(BarCodeTestActivity.this, "内容不能为空！",
+								Toast.LENGTH_SHORT).show();
+					}
+					
+				} catch (WriterException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+    }
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		//处理扫描结果（在界面上显示）
+		if (resultCode == RESULT_OK) {
+			Bundle bundle = data.getExtras();
+			String scanResult = bundle.getString("result");
+//			resultTextView.setText(scanResult);
+			//解决乱码
+			try {
+				String code = new String(scanResult.getBytes("ISO-8859-1"),
+						"GB2312");
+				String utf8 = new String(scanResult.getBytes("ISO-8859-1"),
+						"UTF-8");
+				if (code.equals(utf8)) {
+					code = scanResult;
+				}
+				resultTextView.setText(code);
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+}
